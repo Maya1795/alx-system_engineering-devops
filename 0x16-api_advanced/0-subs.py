82a4c9e0153773e60998cbb66c21ae2962623296
Maya1795 <mayasameh18@gmail.com>
2024-06-19 00:21:27 +0000

    yarabb

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,10 +6,10 @@ def number_of_subscribers(subreddit):
     """return total of subscribtion"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-            "user-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+            "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
-    result = response.json().get("data")
-    return result.get("subscribers")
+    results = response.json().get("data")
+    return results.get("subscribers")
