18cb62e5a7e10f942bee650d85320ffd01adae12
Maya1795 <mayasameh18@gmail.com>
2024-06-19 00:12:47 +0000

    besmallah

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,7 +4,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     """return total of subscribtion"""
-    url = "https://www.reddit.com/r/{}/about.joson".format(subreddit)
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
             "user-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
