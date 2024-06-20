99efa6144a56b78c46a60720f192d03c83b2a08c
Maya1795 <mayasameh18@gmail.com>
2024-06-20 21:26:44 +0000

    yarabbbbb

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-"function to query subcribation"
 import requests
 
 def number_of_subscribers(subreddit):
-    """return total of subscribtion"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-            "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
+    """Return the total number of subscribers for a given subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'myUserAgent'}
+    
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
-            results = response.json()
-            return results.get("data", {}).get("subscribers", 0)
+            data = response.json()
+            return data['data']['subscribers']
         else:
             return 0
-    except requests.RequestException:
+    except Exception as e:
         return 0
-    except ValueError:
-        return 0
+
