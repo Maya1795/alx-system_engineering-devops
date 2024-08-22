e16cb57929aeaf0d806d6f504007236c388817bb
Maya1795 <mayasameh18@gmail.com>
2024-08-22 19:01:43 +0000

    yarabbb

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,11 @@
+# reconfigure the os
+
+exec { 'increase-hard-file-limit-holberton-user':
+  command => 'sed -i "/holberton hard/s/4/50000/" /etc/security/limits.conf',
+  path   => '/usr/local/bin/:/bin/:'
+}
+
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path  => '/usr/local/bin/:/bin/'
+}
