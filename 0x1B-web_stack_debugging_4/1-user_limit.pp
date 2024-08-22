f007f4ef8c16b08be484c53b3c1bcbe8d33cefa9
Maya1795 <mayasameh18@gmail.com>
2024-08-22 19:11:04 +0000

    yarabbbbb

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,11 +1,11 @@
-# reconfigure the os
+# reconfigure the os 'holberton' to login and open the file
 
 exec { 'increase-hard-file-limit-holberton-user':
-  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
-  path   => '/usr/local/bin/:/bin/:'
+  command => 'sed -i "/^holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/:'
 }
 
 exec { 'increase-soft-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
-  path  => '/usr/local/bin/:/bin/'
+  command => 'sed -i "/^holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 }
